e958fcaf756e3cd4b67b9100de98e6710e221096
Felipe Marques <felipe.comarques1208@gmail.com>
2023-03-18 20:37:50 -0300

    Adicionado colisão
    Adicionado colisão, OnCollisionEnter2D e também OnTriggerEnter2D

---
+++ Assets/Collision.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log("You collided with something!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("You trigger something!");
+    }
+}
